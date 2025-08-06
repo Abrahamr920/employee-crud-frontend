7aa9c9e4074842f2fc2695c07a096960bf9d8826
Abraham Rodriguez Laptop <abrahamr920@gmail.com>
2025-08-05 20:15:21 -0600

    feat: mejorar configuración de la aplicación añadiendo soporte para LocationStrategy y HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+    provideHttpClient(withFetch()),
+  ],
 };
