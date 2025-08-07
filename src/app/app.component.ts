eac36d6c7722b002fa16c330bf64fef118419b9c
Abraham Rodriguez <abrahamr920@gmail.com>
2025-08-06 19:28:39 -0600

    convertir tabla y formulario en componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeesFormComponent } from './employees/components/employees-form/employees-form.component';
 import { EmployeesTableComponent } from './employees/components/employees-table/employees-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeesTableComponent],
+  imports: [RouterOutlet, EmployeesTableComponent, EmployeesFormComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
